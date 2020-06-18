1f6dcd9059fde58bbdbf9336dea1cf52b3a14d9f
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-06-17 20:07:19 -0400

    Met project MVP

--- client/src/App.js
+++ client/src/App.js
@@ -3,37 +3,45 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
-import axios from 'axios';
+import axios from "axios";
+import UpdateForm from "./Movies/UpdateForm";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
+  const [refresh, setRefresh] = useState(true);
 
   const getMovieList = () => {
     axios
       .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
-      .catch(err => console.log(err.response));
+      .then((res) => setMovieList(res.data))
+      .catch((err) => console.log(err.response))
+      .finally(() => {
+        setRefresh(false);
+      });
   };
 
-  const addToSavedList = movie => {
+  const addToSavedList = (movie) => {
     setSavedList([...savedList, movie]);
   };
 
   useEffect(() => {
     getMovieList();
-  }, []);
+  }, [refresh]);
 
   return (
     <>
       <SavedList list={savedList} />
-
       <Route exact path="/">
         <MovieList movies={movieList} />
       </Route>
 
       <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
+        <Movie addToSavedList={addToSavedList} setRefresh={setRefresh} />
+      </Route>
+
+      <Route path="/update-movie/:id">
+        <UpdateForm setMovieList={setMovieList} setRefresh={setRefresh} />
       </Route>
     </>
   );
